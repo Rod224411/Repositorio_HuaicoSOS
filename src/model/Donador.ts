67d33145d2e33a84be84e6430d36bb89f51fe32d
Rod224411 <103529855+Rod224411@users.noreply.github.com>
2023-04-30 23:19:24 -0500

    Add files via upload

---
+++ src/model/Donador.ts
@@ -0,0 +1,11 @@
+export class Donador{
+  id:number=0
+  dni:number=0
+  nombres:string=""
+  apellidos:string=""
+  edad:number=0
+  telefono:number=0
+  contrasena:string=""
+  correo:string=""
+  ong:boolean=false
+}
